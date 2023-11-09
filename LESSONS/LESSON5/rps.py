26c77f341a5477ddea90a0096b36e022ea9619aa
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-09 22:51:30 +0100

    first game on Python

---
+++ LESSONS/LESSON5/rps.py
@@ -0,0 +1,42 @@
+import sys
+import random 
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+# different ways to access value on Enum class
+# print(RPS(2))
+# print(RPS.ROCK)
+# print(RPS['ROCK'])
+# print(RPS.ROCK.value)
+# sys.exit()
+
+print('')
+userchoice = input('Make a choose:\n 1-for Rock\n 2-for Paper\n 3-for Scissors:\n\n')
+
+player = int(userchoice)
+
+if player < 1 | player > 3:
+    sys.exit('You must enter 1,2 or 3')
+
+computerchoice = random.choice('123')
+computer = int(computerchoice)
+
+print('')
+print('You chose ' + str(RPS(player)).replace('RPS.','') + '.')
+print('Python chose ' + str(RPS(computer)).replace('RPS.','') + '.')
+print('')
+
+if player == 1 and computer == 3 :
+    print('🍾You win')
+elif player == 2 and computer == 3 :
+    print('🍾You win')
+elif player == 3 and computer == 2 :
+    print('🍾You win')
+elif player == computer :
+    print('😲Tie game')
+else :
+    print('🐍Phyton win')
